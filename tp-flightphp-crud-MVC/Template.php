0ed3871a205655260cf57e017d61a355e576a515
heniintsoa <henintsoaandria49@gmail.com>
2025-07-07 20:21:35 +0300

    Add files via upload

---
+++ tp-flightphp-crud-MVC/Template.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil - Gestion de Comptabilite</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <nav>
+                <a href="" class=""></a>
+                    <ul class="menu">
+                        <li><a href="">Accueil</a></li>
+                        <li><a href="">Ajout-Fond</a></li>
+                        <li><a href="">Insertion-types-de-prets</a></li>
+                        <li><a href="">Insertion-prets</a></li>
+                        <li><a href="">Argent-Rendue</a></li>
+                        <li><a href="">Etat-client</a></li>
+                        <li><a href="">Benefice-Interet</a></li>
+                    </ul>
+            </nav>
+        </div>
+    </header>
+
+    <main>
+        <?php include($page.'.php'); ?>
+    </main>
+
+    <footer>
+        <p>&copy; ...</p>
+    </footer>
+</body>
+</html>
---
+++ tp-flightphp-crud-MVC/sql/script.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE EF CHARACTER SET utf8mb4;
+
+USE EF;
+
+CREATE TABLE t_EF(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    fond FLOAT(10, 2) NOT NULL
+);
+
+CREATE TABLE t_typepret(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(50) NOT NULL,
+    taux-mois INT
+);
+
+CREATE TABLE t_renbourssement(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    idpret INT,
+    volanaverina FLOAT(10, 2) NOT NULL,
+    date DATE NOT NULL,
+
+    FOREIGN KEY (idpret) REFERENCES t_pret(id)
+);
+
+CREATE TABLE t_client(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(50) NOT NULL,
+    argentGlobal FLOAT(10, 2) NOT NULL
+);
+
+CREATE TABLE t_pret(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    idclient INT,
+    idtypepret INT,
+    mois_debut INT NOT NULL,
+    mois_fin INT NOT NULL,
+    montant_par_mois FLOAT(10, 2) NOT NULL,
+
+    FOREIGN KEY (idclient) REFERENCES t_client(id),
+    FOREIGN KEY (idtypepret) REFERENCES t_typepret(id)
+);
+
